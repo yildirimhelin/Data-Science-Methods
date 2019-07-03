6c96fa74bf1aaad60a83ad282c57c42f38558553
Helin Yildirim <48429155+yildirimhelin@users.noreply.github.com>
2019-07-03 09:39:39 +0300

    Update METHODS.py

--- METHODS.py
+++ METHODS.py
@@ -1,1 +1,10 @@
 
+import pandas as pd
+import numpy as np
+
+#Gives the percentage of missing values for all variables of 'df'. 
+def missing_percent(df):
+    missing = df.isnull().sum().sort_values(ascending=False)
+    percent = (df.isnull().sum()/df.isnull().count()).sort_values(ascending=False)
+    missing_data = pd.concat([missing, percent], axis=1, keys=['Missing', 'Percent'])
+    return missing_data
